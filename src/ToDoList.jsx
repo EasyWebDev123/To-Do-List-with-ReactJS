836b1ecabbc1e5a5c2fd697f630a76e2af73deca
Abiona Israel <israelabiona44@gmail.com>
2026-05-26 12:12:31 +0100

    feat: enhance ToDoList functionality with task management features

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -10,15 +10,31 @@ function ToDoList(){
   }
 
   function addTask(){
+    if(newTask.trim() !== ""){
+       setTasks(t => [...t, newTask]);
+    setNewTask("");
+    }
   }
 
   function deleteTask(index){
+    const updatedTask = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTask)
   }
 
   function moveTaskUp(index){
+    if(index > 0){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
 
   function moveTaskDown(index){
+    if(index < tasks.length -1){
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
   }
 
   return(
@@ -27,8 +43,23 @@ function ToDoList(){
 
       <div>
         <input type="text" placeholder="Enter a task..." value={newTask} onChange={inputChange} aria-label="t" />
+
         <button className="add-button" onClick={addTask}>ADD</button>
         </div>
+
+        <ol>
+          {tasks.map((task, index) =>
+          <li key={index}>
+            <span className="text">{task}</span>
+
+            <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+
+            <button className="move-button" onClick={() => moveTaskUp(index)}>👆</button>
+
+            <button className="move-button" onClick={() => moveTaskDown(index)}>👇</button>
+          </li>
+          )}
+        </ol>
 
     </div>
   );
